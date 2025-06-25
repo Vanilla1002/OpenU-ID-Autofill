1d502b8acecbae041c46c305bfe632ad7c98ecea
Vanilla1002 <Shukerharel@gmail.com>
2025-06-25 12:03:20 +0300

    save on enter

--- popup.js
+++ popup.js
@@ -6,15 +6,27 @@ function isValidId(id) {
   return /^[0-9]{9}$/.test(id);
 }
 
-saveBtn.addEventListener("click", () => {
+function saveId() {
   const id = input.value.trim();
 
   if (!isValidId(id)) {
     status1.textContent = "Enter a valid ID.";
     return;
   }
+
   chrome.storage.sync.set({ ouId: id }, () => {
     status1.textContent = "Saved!";
-    setTimeout(() => ((status1.textContent = ""), window.close()), 2000);
+    setTimeout(() => {
+      status1.textContent = "";
+      window.close();
+    }, 2000);
   });
+}
+
+saveBtn.addEventListener("click", saveId);
+
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    saveId();
+  }
 });
